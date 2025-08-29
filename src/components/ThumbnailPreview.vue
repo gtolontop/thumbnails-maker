6008f3c6e3932f248a1c9f47cb37f107c952341b
Cruck Store <cruckstore+github@gmail.com>
2025-08-29 11:53:53 +0200

    fix: increase title text size and adjust layout in ThumbnailPreview for improved visibility

--- src/components/ThumbnailPreview.vue
+++ src/components/ThumbnailPreview.vue
@@ -12,9 +12,9 @@
           <span v-else class="text-[var(--thumbnail-secondary-text-color)] text-2xl">{{ configStore.handleText }}</span>
         </div>
         <div class="title-container relative">
-          <h1 class="title text-[90px] font-extrabold leading-[1] m-0" style="color: #9EA4BE;">{{ configStore.titleText }}</h1>
-          <div class="reflect-wrapper absolute left-0 w-full overflow-hidden" style="top: calc(100% - 22px); height: 90px;">
-            <h1 class="text-[90px] font-extrabold leading-[1] m-0 transform scale-y-[-1] opacity-30" 
+          <h1 class="title text-[130px] font-extrabold leading-[1] m-0" style="color: #9EA4BE;">{{ configStore.titleText }}</h1>
+          <div class="reflect-wrapper absolute left-0 w-full overflow-hidden" style="top: calc(100% - 32px); height: 130px;">
+            <h1 class="text-[130px] font-extrabold leading-[1] m-0 transform scale-y-[-1] opacity-30" 
                 style="color: #9EA4BE;
                        background: linear-gradient(to bottom, transparent 20%, #9EA4BE 100%); 
                        -webkit-background-clip: text; 
@@ -56,9 +56,9 @@
           </div>
         </div>
         <div class="relative inline-block">
-          <h1 id="text-only-title" class="text-[110px] font-extrabold leading-[1] m-0" style="color: #9EA4BE;">{{ configStore.titleText }}</h1>
-          <div class="absolute left-0 w-full overflow-hidden" style="top: calc(100% - 27px); height: 110px;">
-            <h1 class="text-[110px] font-extrabold leading-[1] m-0 transform scale-y-[-1] opacity-30" 
+          <h1 id="text-only-title" class="text-[130px] font-extrabold leading-[1] m-0" style="color: #9EA4BE;">{{ configStore.titleText }}</h1>
+          <div class="absolute left-0 w-full overflow-hidden" style="top: calc(100% - 32px); height: 130px;">
+            <h1 class="text-[130px] font-extrabold leading-[1] m-0 transform scale-y-[-1] opacity-30" 
                 style="color: #9EA4BE;
                        background: linear-gradient(to bottom, transparent 20%, #9EA4BE 100%); 
                        -webkit-background-clip: text; 
